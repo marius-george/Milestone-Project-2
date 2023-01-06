4b6781bb247983b88c1b75d885d0d820f0463a86
marius-george <marius.olariu@hotmail.com>
2023-01-06 14:24:24 +0000

    Added constant variables to target html elements. Modified newQuote function to add text content when page refreshed. Added event listener tu Random Quote button to change the quotes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
+const mainDiv  = document.getElementById('main-div');
+const mainText  = document.getElementById('main');
+const mainAuthor  = document.getElementById('main-author');
+const mainButton  = document.getElementById('random-quote');
+
 let apiQuote = [];
+
+// Generate random quote
+function newQuote() {
+    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+    mainAuthor.textContent = quote.author;
+    mainText.textContent = quote.text;
+}
 
 async function getQuote() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         let response = await fetch(apiUrl);
         apiQuote = await response.json();
-        // Shows in console quote 244 from API array. I have to create a random function using math.floor and math.random o display random quote. 
-        console.log(apiQuote[243]);
+        newQuote();
     }
     catch (error) {
 
     }
 }
+
+//Event Listeners
+
+mainButton.addEventListener('click', newQuote);
 
 getQuote();
